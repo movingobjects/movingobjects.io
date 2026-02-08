959fdd60b5e4cd43ca361bf90f485f69d3d2b558
Scott Thiessen <scott@movingobjects.io>
2026-02-08 12:44:24 -0800

    Update favicon, and automate favicon generation

--- config/theme.ts
+++ config/theme.ts
@@ -2,6 +2,8 @@ export const THEME = {
   colors: {
     bg: '#292966',
     primary: '#f77',
-    secondary: '#8f8'
+    secondary: '#8f8',
+    logoBg: '#00D96C',
+    logoFg: '#006D46'
   }
 } as const;
---
+++ scripts/generate-favicons.js
@@ -0,0 +1,42 @@
+import favicons from "favicons";
+import fs from "fs/promises";
+import path from "path";
+import { THEME } from "../config/theme.ts";
+
+const src = "./images/favicon.svg";
+const dest = "./public";
+
+const configuration = {
+  path: "/",
+  appName: "Moving Objects",
+  appShortName: "Moving Objects",
+  appDescription: null,
+  background: THEME.colors.logoBg,
+  theme_color: THEME.colors.logoBg,
+  icons: {
+    android: false,
+    appleIcon: [180],
+    appleStartup: false,
+    favicons: true,
+    windows: false,
+    yandex: false
+  }
+};
+
+const response = await favicons(src, configuration);
+await fs.mkdir(dest, { recursive: true });
+await Promise.all(
+  response.images.map(
+    async (image) =>
+      await fs.writeFile(path.join(dest, image.name), image.contents),
+  ),
+);
+await Promise.all(
+  response.files.map(
+    async (file) =>
+      await fs.writeFile(path.join(dest, file.name), file.contents),
+  ),
+);
+
+const sourceSvg = await fs.readFile(src);
+await fs.writeFile(path.join(dest, "favicon.svg"), sourceSvg);
